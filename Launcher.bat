d944883d74dadf961869b20b64be453f0a3ae537
Yashvir Gaming <64865096+YashvirGaming@users.noreply.github.com>
2025-11-01 22:44:31 +0400

    Add files via upload

---
+++ Launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+title DNS Manager Pro 2025 - Yashvir Gaming
+color 0A
+mode con: cols=120 lines=40
+echo.
+echo  🧠 Launching DNS Manager Pro 2025 by Yashvir Gaming...
+echo.
+python dns_manager_pro.py
+echo.
+pause
